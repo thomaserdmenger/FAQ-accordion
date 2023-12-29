761ab75902ecbeaa1f96cc37105622e314074c89
thomaserdmenger <thomas.erdmenger@googlemail.com>
2023-12-29 23:06:27 +0100

    Change FAQ Icon on expand and shrink

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const faqArr = Array.from(faq);
 const expandFaq = (e) => {
   const container = e.target.closest('.card-faq-container');
   const answer = container.querySelector('p');
-  console.log(answer);
-  answer.classList.toggle('card-faq-toggle');
+  answer.classList.toggle('toggle-style');
+  const iconPlus = container.querySelector('.plus');
+  iconPlus.classList.toggle('toggle-style');
+  const iconMinus = container.querySelector('.minus');
+  console.log(iconMinus);
+  iconMinus.classList.toggle('minus-block');
 };
 
 faqArr.map((item) => item.addEventListener('click', expandFaq));
